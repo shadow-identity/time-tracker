4e5aba0513b63b8e86ac19bc9ed7f3873c39361e
Pavel Nedrigailov <pavel.nedrigailov@invenium.io>
2023-02-05 21:51:51 +0100

    Build preparation

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,19 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
+		paths: {
+			base: dev ? '' : '/time-tracker'
+		},
+		adapter: adapter({
+			pages: 'docs',
+			assets: 'docs'
+		})
 	}
 };
 
